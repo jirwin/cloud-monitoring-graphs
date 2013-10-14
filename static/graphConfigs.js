2f23babf2ffec32fa03a0524886d00e29d7c1a14
Justin Gallardo <justin.gallardo@rackspace.com>
2013-10-13 21:19:44 -0700

    Acutally graph data. /dashboard.html is a static html file that asyncronously grabs graph data and renders it. The graphs rendered can be configured in static/graphConfigs.js

---
+++ static/graphConfigs.js
@@ -0,0 +1,73 @@
+graphConfigs = {
+  "averageDeploys": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "average_deploys",
+    "type": "line",
+    "graphKey": "deploys",
+    "title": "Average Deploys",
+    "element": "#deploy-graph",
+    "color": "red",
+    "unit": "deploys"
+  },
+
+  "totalDeploys": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "total_deploys",
+    "type": "line",
+    "graphKey": "deploys",
+    "title": "Total Deploys",
+    "element": "#deploy-graph",
+    "color": "green",
+    "unit": "deploys"
+  },
+
+  "totalUsers": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "total_users",
+    "type": "line",
+    "graphKey": "users",
+    "title": "Total Users",
+    "element": "#users-graph",
+    "color": "green",
+    "unit": "users"
+  },
+
+  "averageUsers": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "average_users",
+    "type": "line",
+    "graphKey": "users",
+    "title": "Average Users",
+    "element": "#users-graph",
+    "color": "red",
+    "unit": "users"
+  },
+
+  "totalSpeed": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "total_speed",
+    "type": "line",
+    "graphKey": "speed",
+    "title": "Total speed",
+    "element": "#speed-graph",
+    "color": "green",
+    "unit": "dps"
+  },
+
+  "averageSpeed": {
+    "entity": "enzgg8zESh",
+    "check": "chQPEZpHzY",
+    "metric": "average_speed",
+    "type": "line",
+    "graphKey": "speed",
+    "title": "Average speed",
+    "element": "#speed-graph",
+    "color": "red",
+    "unit": "dps"
+  }
+}
